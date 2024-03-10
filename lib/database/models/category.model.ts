a3a5c8731c8cd2115bf765b82e88b3fe081d6f23
Paras <paras@give.do>
2024-03-10 21:08:04 +0530

    uploading files

---
+++ lib/database/models/category.model.ts
@@ -0,0 +1,15 @@
+import { Schema, models, model, Document } from "mongoose";
+
+export interface ICategory extends Document {
+  _id: string;
+  name: string;
+}
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true, unique: true },
+});
+
+const Category =
+  models.Category || model<ICategory>("Category", CategorySchema);
+
+export default Category;
